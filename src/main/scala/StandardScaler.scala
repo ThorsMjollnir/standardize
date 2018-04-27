291892f414b11138c7901d9ff89a14389d011413
Tim Osadchiy <t.osadchiy@ncl.ac.uk>
2018-04-27 18:05:06 +0100

    first commit

---
+++ src/main/scala/StandardScaler.scala
@@ -0,0 +1,75 @@
+/**
+  * Created by Tim Osadchiy on 27/04/2018.
+  */
+
+object StandardScaler {
+
+  def findInconsistency(matrix: Vector[Vector[Double]]): Option[Int] = {
+    val size = matrix.head.size
+    matrix.zipWithIndex.find(v => v._1.size != size).map(v => v._2)
+  }
+
+  def validateMatrix(matrix: Vector[Vector[Double]]) = {
+    if (matrix.isEmpty) {
+      throw new IllegalArgumentException("Matrix is empty")
+    }
+
+    val inc = StandardScaler.findInconsistency(matrix)
+    if (inc.isDefined) {
+      throw new IllegalArgumentException(s"Matrix shape is not consistent at ${inc.getOrElse(-1)}")
+    }
+  }
+
+  def validateTransformedMatrix(matrix: Vector[Vector[Double]], size: Int) = {
+    if (matrix.isEmpty) {
+      throw new IllegalArgumentException("Matrix is empty")
+    }
+
+    val inc = matrix.find(v => v.size != size)
+    if (inc.isDefined) {
+      throw new IllegalArgumentException(s"Vector at index ${inc.getOrElse(-1)} is of unexpected size. Expected size is $size")
+    }
+  }
+
+  def transpose(matrix: Vector[Vector[Double]]): Vector[Vector[Double]] = {
+    matrix.head.indices.map(i => matrix.map(_ (i))).toVector
+  }
+
+  def mean(matrix: Vector[Vector[Double]]): Vector[Double] = {
+    val tr = transpose(matrix)
+    tr.map(v => v.sum / v.size)
+  }
+
+  def std(matrix: Vector[Vector[Double]]) = {
+    val tr = transpose(matrix)
+    val mn = mean(matrix)
+    tr.zipWithIndex.map { vi =>
+      Math.sqrt(vi._1.map(x => Math.pow(x - mn(vi._2), 2)).sum / vi._1.size)
+    }
+  }
+
+}
+
+case class StandardScaler(withMean: Boolean = true, withStd: Boolean = true) {
+
+  def fit(matrix: Vector[Vector[Double]]) = {
+    StandardScaler.validateMatrix(matrix)
+    ScalerModel(withMean, withStd, StandardScaler.mean(matrix), StandardScaler.std(matrix), matrix.head.size)
+  }
+
+}
+
+case class ScalerModel(withMean: Boolean, withStd: Boolean, mean: Vector[Double], std: Vector[Double], vectorSize: Int) {
+
+  def transform(matrix: Vector[Vector[Double]]) = {
+    StandardScaler.validateTransformedMatrix(matrix, vectorSize)
+    matrix.map { vector =>
+      vector.zipWithIndex.map { xi =>
+        val mn = if (withMean) mean(xi._2) else 0d
+        val s = if (withStd) std(xi._2) else 1d
+        (xi._1 - mn) / s
+      }
+    }
+  }
+
+}
---
+++ src/test/scala/StandardScaleTest.scala
@@ -0,0 +1,85 @@
+import org.scalactic.Equality
+import org.scalatest.FunSuite
+
+/**
+  * Created by Tim Osadchiy on 27/04/2018.
+  */
+class StandardScaleTest extends FunSuite {
+
+  val inMatrix = Vector(
+    Vector(2d, 100d),
+    Vector(10d, 200d),
+    Vector(4d, 120d),
+    Vector(6d, 130d)
+  )
+
+  implicit val doubleEquality: Equality[Double] =
+    (a: Double, b: Any) => Math.abs(a - b.asInstanceOf[Double]) < 1e-8
+
+  def assertAnswer(expected: Vector[Vector[Double]], actual: Vector[Vector[Double]]) =
+    expected.zipWithIndex.forall { ev =>
+      ev._1.zipWithIndex.forall(sev => actual(ev._2)(sev._2) === sev._1)
+    }
+
+  test("Mean is calculated correctly") {
+
+    val answer = Vector(5.5, 137.5)
+    val mean = StandardScaler.mean(inMatrix)
+    assert(mean.zipWithIndex.forall(i => i._1 === answer(i._2)))
+
+  }
+
+  test("Std is calculated correctly") {
+
+    val answer = Vector(2.95803989, 37.66629793)
+    val std = StandardScaler.std(inMatrix)
+    assert(std.zipWithIndex.forall(i => i._1 === answer(i._2)))
+
+  }
+
+  test("Transform with std only") {
+
+    val answer = Vector(
+      Vector(0.6761234, 2.65489325),
+      Vector(3.38061702, 5.30978649),
+      Vector(1.35224681, 3.1858719),
+      Vector(2.02837021, 3.45136122)
+    )
+
+    val model = StandardScaler(withMean = false).fit(inMatrix)
+    val transformed = model.transform(inMatrix)
+    assert(assertAnswer(answer, transformed))
+
+  }
+
+  test("Transform with mean only") {
+
+    val answer = Vector(
+      Vector(-3.5, -37.5),
+      Vector(4.5, 62.5),
+      Vector(-1.5, -17.5),
+      Vector(0.5, -7.5)
+    )
+
+    val model = StandardScaler(withStd = false).fit(inMatrix)
+    val transformed = model.transform(inMatrix)
+    assert(assertAnswer(answer, transformed))
+
+  }
+
+  test("Transform with std and mean") {
+
+    val answer = Vector(
+      Vector(-1.18321596, -0.99558497),
+      Vector(1.52127766, 1.65930828),
+      Vector(-0.50709255, -0.46460632),
+      Vector(0.16903085, -0.19911699)
+    )
+
+    val model = StandardScaler().fit(inMatrix)
+    val transformed = model.transform(inMatrix)
+    assert(assertAnswer(answer, transformed))
+
+  }
+
+}
